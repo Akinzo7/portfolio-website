b778457d8b0eabe12d437f790752fb7830eda451
Akinwumi Akinfemi <kingfanak@gmail.com>
2025-11-02 19:32:54 +0100

    Add hamburger menu

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ links.forEach(link => {
         link.classList.add('active');
     });
 });
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
