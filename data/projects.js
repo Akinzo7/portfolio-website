a6bf402b709d58c95b57530324030c5e5c6d4638
Akinwumi Akinfemi <kingfanak@gmail.com>
2025-11-23 18:27:58 +0100

    Add project section with dynamic rendering and improve layout responsiveness

---
+++ data/projects.js
@@ -0,0 +1,27 @@
+const projects = [
+  {
+    id: 1,
+    title: "Professional Portfolio website",
+    img: "./images/portfolio-website.png",
+    stack: ["html", "css", "javascript"],
+    stack_type: "frontend",
+    progress: "ongoing",
+    liveLink: "#",
+    githubLink: "#",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 2,
+    title: "Weather App",
+    img: "./weather-app.png",
+    stack_type: "backend",
+    progress: "completed",
+    stack: ["html", "css", "javascript", "api"],
+    liveLink: "#",
+    githubLink: "#",
+    description: "A simple weather app that fetches weather data from an API.",
+  },
+];
+
+export default projects;
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import projects from "./data/projects.js";
+
+// Smooth scrolling and active link highlighting
+
 const links = document.querySelectorAll(".navbar-links a, .menu-links a");
 links.forEach((link) => {
   link.addEventListener("click", (event) => {
@@ -16,7 +20,7 @@ links.forEach((link) => {
   });
 });
 
-function toggleMenu() {
+window.toggleMenu = function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
@@ -30,4 +34,43 @@ window.addEventListener("scroll", () => {
 
 
 
+// Projects iteration
 
+let projectCardHTML="";
+projects.forEach((project) => {
+    console.log(project);
+    const { id, title, img, stack,stack_type, progress, liveLink, githubLink, description } = project;
+
+  projectCardHTML += `
+  <div class="container">
+     <div class="project_img">
+        <img src="${img}" alt="project of ${title}" />
+      </div>
+      <div class="project">
+        <div class="project_status">
+          <div class="stack_type">${stack_type}</div>
+          <div class="project_progress">${progress}</div>
+        </div>
+        <div class="project_details">
+          <h2 class="project_title">${title} </h2>
+          <p class="project_info">
+            ${description}
+          </p>
+        </div>
+        <div class="project_stack">
+          ${stack.map(tech => `<p>${tech}</p>`).join('')}
+        </div>
+        <div class="project_display">
+          <a href="${liveLink}">view live</a
+          ><a class="github_icon" href="${githubLink}"
+            >
+            <img src="./Assets/github-mark-white.png" alt="" 
+          /></a>
+        </div>
+      </div>
+    </div>
+  `;
+});
+
+const container = document.querySelector(".js-grid-container");
+container.innerHTML = projectCardHTML;  
